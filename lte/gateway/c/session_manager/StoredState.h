a6d9bfdee5543d91be9845f16f45d5f200df3a03
Oriol Batalla <uri200@hotmail.com>
2020-10-01 22:03:44 -0700

    Revert "Remove TERMINATION_SCHEDULED (#2989)"
    This reverts commit faf0aa101095b101b80228bf5fec1a6cfbab3a04.

--- lte/gateway/c/session_manager/StoredState.h
+++ lte/gateway/c/session_manager/StoredState.h
@@ -108,11 +108,16 @@ enum GrantTrackingType {
 
 /**
  * State transitions of a session:
- * SESSION_ACTIVE
- *       |
- *       |
- *       |
- *       V
+ * SESSION_ACTIVE  ---------
+ *       |                  \
+ *       |                   \
+ *       |                    \
+ *       |                     \
+ *       | (start_termination)  SESSION_TERMINATION_SCHEDULED
+ *       |                      /
+ *       |                     /
+ *       |                    /
+ *       V                   V
  * SESSION_RELEASED
  *       |
  *       | (PipelineD enforcement flows get deleted OR forced timeout)
@@ -121,14 +126,15 @@ enum GrantTrackingType {
  * SESSION_TERMINATED
  */
 enum SessionFsmState {
-  SESSION_ACTIVE     = 0,
-  SESSION_TERMINATED = 4,
-  SESSION_RELEASED   = 6,
-  CREATING           = 7,
-  CREATED            = 8,
-  ACTIVE             = 9,
-  INACTIVE           = 10,
-  RELEASE            = 11,
+  SESSION_ACTIVE                = 0,
+  SESSION_TERMINATED            = 4,
+  SESSION_TERMINATION_SCHEDULED = 5,
+  SESSION_RELEASED              = 6,
+  CREATING                      = 7,
+  CREATED                       = 8,
+  ACTIVE                        = 9,
+  INACTIVE                      = 10,
+  RELEASE                       = 11,
 };
 
 struct StoredSessionCredit {
